6b9eca85f3a616dcc3b37dc01de12b7a5b479669
kek <oleh.petryk@gmail.com>
2017-12-08 18:39:26 +0200

    c

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-
-let i=0;
-while(i<100){
-    document.write(i+` `);
+let i = 0;
+while (i < 100) {
+  document.write(` ${i} `);
+  i++;
+}
+document.write(`<br>`);
+i=0;
+do{
+    document.write(` ${i} `);
     i++
 }
+while(i<100)
