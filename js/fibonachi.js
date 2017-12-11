4784d07d822aaf4e0cd542ac4e3b40635f380fd2
kek <oleh.petryk@gmail.com>
2017-12-11 16:54:29 +0200

    c

--- js/fibonachi.js
+++ js/fibonachi.js
@@ -3,7 +3,7 @@ function fibonacciLoop(num)
   let a = 1,
     b = 0,
     temp;
-  for (let i=num; num > 0; i--) 
+  for (; num > 0; num--) 
   {
     temp = a;
     a = a + b;
@@ -24,6 +24,6 @@ function fibRec(n) {
   return n <= 2 ? 1 : fibRec(n - 1) + fibRec(n - 2);
 }
 var a = prompt("введіть порядковий номер числа фібоначі, яке буде розраховано");
-document.write("Цикл: " + fibonacciCycle(a) + "<br>");
+document.write("Цикл: " + fibonacciLoop(a) + "<br>");
 document.write("Масив: " + fibonacciArray(a) + "<br>");
 document.write("Рекурсія: " + fibRec(a));
